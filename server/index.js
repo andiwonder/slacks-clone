4c65300d836c089748cba3c669f21d147264f04b
Swapnil Kasliwal <skasliwal12@gmail.com>
2018-04-16 14:49:08 -0400

    graphql setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-index.js;
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const PORT = 3000;
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const myGraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+const ENDPOINT = '/graphql';
+
+app.use(ENDPOINT, bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: ENDPOINT }));
+
+app.listen(PORT);
